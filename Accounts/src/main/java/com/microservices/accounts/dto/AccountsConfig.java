36a18e88cccbf81410bf85668c155e7396084f6b
itsakram101 <akram.iguernait@gmail.com>
2024-05-19 22:56:38 +0100

    tested remote cloud config, added sample docker composer & rabbitmq image for future proper set up

--- Accounts/src/main/java/com/microservices/accounts/dto/AccountsConfig.java
+++ Accounts/src/main/java/com/microservices/accounts/dto/AccountsConfig.java
@@ -1,11 +1,20 @@
 package com.microservices.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsConfig(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsConfig{
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
