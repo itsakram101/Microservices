cecd71e654c94de8c5625f4e121a9f3691c396c6
itsakram101 <akram.iguernait@gmail.com>
2024-07-06 15:16:09 +0100

    Implemented retry pattern in gateway server, added condition to not repopulate correlation-id if it exists

--- gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
@@ -6,9 +6,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.http.HttpMethod;
 
 
-
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -31,7 +32,10 @@ public class GatewayserverApplication {
                 .route(p -> p
                         .path("/test/loans/**")
                         .filters( f -> f.rewritePath("/test/loans/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000), 2, true)))
                         .uri("lb://LOANS"))
                 .route(p -> p
                         .path("/test/cards/**")
